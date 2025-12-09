661046b847efd3268f6a5dc873c7208213516fa1
Babismam <babis1993@gmail.com>
2025-12-09 10:24:08 +0200

    Gym Frontend 9/12/25

---
+++ src/config/api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { getToken } from "../services/authService";
+
+const API_URL = "http://localhost:8080/gymmanagement/api";
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
